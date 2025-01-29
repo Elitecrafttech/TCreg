48205ab2966cc1e9cdc929d776271a4e3554841f
Elitecrafttech <elitecrafttech@gmail.com>
2025-01-29 15:55:13 +0100

    Add files via upload

---
+++ company-project/renderer/script.js
@@ -0,0 +1,17 @@
+// function handleSelectChange(event) {
+//     const selectedValue = event.target.value;
+//     if (selectedValue) {
+//         console.log(`Selected Role: ${selectedValue}`);
+        
+//     }
+// }
+// document.querySelectorAll('.opt1').forEach(select => {
+//     select.addEventListener('change', handleSelectChange);
+// });
+
+
+
+
+
+/* == register javascript */
+
